ce1ac283a07503561f15de51c4fed37f5b652926
choigyulee <cgl1409@gmail.com>
2024-11-23 00:40:34 +0900

    fix<Carousel>UI수정

--- src/components/OngoingSummit/Carousel.js
+++ src/components/OngoingSummit/Carousel.js
@@ -33,7 +33,6 @@ const Carousel = ({ items, title }) => {
 
   const totalPages = Math.ceil(items.length / itemsPerPage); // 전체 페이지 수
 
-  const shouldHideNavigation = items.length <= itemsPerPage;
 
   return (
     <CarouselContainer>
@@ -54,7 +53,7 @@ const Carousel = ({ items, title }) => {
         ))}
       </CarouselWrapper>
 
-      {!shouldHideNavigation && (
+      {!items.length < 1 && (
         <>
           <LeftButton onClick={handlePrev} active={currentIndex > 0}>
             <BsChevronLeft size={32} />
@@ -68,17 +67,20 @@ const Carousel = ({ items, title }) => {
         </>
       )}
 
-      {!shouldHideNavigation && items.length > 3 && (
-        <CarouselNav>
-          {Array.from({ length: totalPages }).map((_, index) => (
+      {/* 3개 이하일 때는 1개의 dot만 보여주고 active 상태로 표시 */}
+      <CarouselNav>
+        {items.length <= 3 ? (
+          <CarouselDot active={true} />
+        ) : (
+          Array.from({ length: totalPages }).map((_, index) => (
             <CarouselDot
               key={index}
               active={index === currentIndex}
               onClick={() => handleDotClick(index)}
             />
-          ))}
-        </CarouselNav>
-      )}
+          ))
+        )}
+      </CarouselNav>
     </CarouselContainer>
   );
 };
@@ -102,6 +104,8 @@ const Title = styled.div`
   position: absolute;
   left: 80px;
   top: 40px;
+
+  color: ${themeGet('color.500')};
   font-size: ${themeGet("fonts.h3.size")};
   font-weight: bold;
 `;
@@ -109,16 +113,27 @@ const Title = styled.div`
 const CarouselWrapper = styled.div`
   display: flex;
   transition: transform 0.3s ease-in-out;
-  height: 500px;
+  height: 560px;
   align-items: center;
+
+  gap: 40px;
 `;
 
+const CarouselCardStyled = styled.div`
+  background-color: white;
+  border-radius: 20px;
+  box-shadow: 1px 1px 10px 1px #00000040;
+  width: 314px; // 기존 Carousel 스타일에 맞춘 너비
+  height: 342px; // 기존 Carousel 카드 높이에 맞춘 높이
+  overflow: hidden;
+  margin-right: 20px;
+`;
 
 const CarouselButton = styled.button`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  background-color: ${themeGet("color.main_light")};
+  background-color: ${themeGet("color.salmon")};
   color: ${themeGet("color.white")};
   border: none;
   cursor: pointer;
@@ -156,7 +171,7 @@ const RightButton = styled(CarouselButton)`
 
 const CarouselNav = styled.div`
   position: absolute;
-  bottom: 10px;
+  bottom: 30px;
   left: 50%;
   transform: translateX(-50%);
   display: flex;
