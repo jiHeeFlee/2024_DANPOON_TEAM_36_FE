e4bff9b01c019f0669416ffa657a0e9a3804bba8
choigyulee <cgl1409@gmail.com>
2024-11-24 02:53:15 +0900

    style<Main>UI수정

--- src/constants/ProposeCardMockup.js
+++ src/constants/ProposeCardMockup.js
@@ -1,26 +1,8 @@
 export const ProposeCardMockup = [
   {
-    name: "최우혁",
+    name: "최규리",
     title: "대표",
-    phone: "010-6247-1409",
-    email: "WHInv@gmail.com",
-  },
-  {
-    name: "이영수",
-    title: "대표",
-    phone: "010-1234-5678",
-    email: "leeyoungsu@gmail.com",
-  },
-  {
-    name: "김태훈",
-    title: "CTO",
-    phone: "010-8765-4321",
-    email: "kimtaehun@example.com",
-  },
-  {
-    name: "박진영",
-    title: "이사",
-    phone: "010-1122-3344",
-    email: "parkjinyoung@example.com",
+    phone: "010-4991-1409",
+    email: "cgl1409@gmail.com",
   },
 ];
